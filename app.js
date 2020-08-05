ebb35d63b05e449097855237e473375b2d45c0a1
HossamMuhammedOmar <hossamomar9876@gmail.com>
2020-08-05 13:13:26 +0200

    some changes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Selector
-const todoInput = document.querySelector(".todo-input");
-const todoButton = document.querySelector(".todo-btn");
-const todoList = document.querySelector(".todo-list");
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-btn');
+const todoList = document.querySelector('.todo-list');
 
 // Event Listener
-todoButton.addEventListener("click", addTodo);
+todoButton.addEventListener('click', addTodo);
 
 // Function
 function addTodo(event) {
@@ -12,30 +12,30 @@ function addTodo(event) {
   event.preventDefault();
 
   // Create Todo Div
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
 
   // Create li
-  const todoLi = document.createElement("li");
+  const todoLi = document.createElement('li');
   todoLi.innerText = todoInput.value;
-  todoLi.classList.add("todo-item");
+  todoLi.classList.add('todo-item');
   todoDiv.appendChild(todoLi);
 
   // Create Completed Button
-  const completedButton = document.createElement("button");
+  const completedButton = document.createElement('button');
   completedButton.innerHTML = '<i class="fas fa-check-circle"></i>';
-  completedButton.classList.add("complete-btn");
+  completedButton.classList.add('complete-btn');
   todoDiv.appendChild(completedButton);
 
   // Create Delete Button
-  const deletedButton = document.createElement("button");
+  const deletedButton = document.createElement('button');
   deletedButton.innerHTML = '<i class="fas fa-trash"></i>';
-  deletedButton.classList.add("delete-btn");
+  deletedButton.classList.add('delete-btn');
   todoDiv.appendChild(deletedButton);
 
   //   Create UL
   todoList.appendChild(todoDiv);
 
   //  Clear Todo Input Value
-  todoInput.value = "";
+  todoInput.value = '';
 }
